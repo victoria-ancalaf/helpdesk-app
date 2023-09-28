69ced11569862872ea7a41bd7b9870a773e1ed31
Victoria Ancalaf <victoria.ancalaf@gmail.com>
2023-09-28 18:40:02 -0300

    base navbar

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "./logo.png";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <Image src={Logo} alt="Helpdesk Logo" width={70} quality={100} placeholder="blur" />
+      <h1>Helpdesk</h1>
+      <Link href="/">Dashboard</Link>
+      <Link href="/tickets">Tickets</Link>
+    </nav>
+  );
+}
