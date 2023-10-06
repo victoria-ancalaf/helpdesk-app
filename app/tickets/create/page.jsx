f2b908c0c375ab3824dc524ba6f637c911005cfd
Victoria Ancalaf <victoria.ancalaf@gmail.com>
2023-10-06 18:36:07 -0300

    Create client form component

---
+++ app/tickets/create/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import CreateForm from "./createForm";
+
+export default function CreateTicket() {
+  return (
+    <main>
+      <h2 className="text-primary text-center">Add a New Ticket</h2>
+      <CreateForm />
+    </main>
+  );
+}
